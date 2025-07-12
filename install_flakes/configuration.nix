18c48e2ebf9c0a73fd66b12b3e277bd46bfc8c61
uff20xd <donut121008@gmail.com>
2025-07-12 16:53:02 +0200

    k

--- install_flakes/configuration.nix
+++ install_flakes/configuration.nix
@@ -1,83 +1,6 @@
-
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ pkgs, lib, ... }:
-
-let
-in 
+{pkgs, modulesPath, ...}: 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix 
-    ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.kernelModules = [ "amdgpu" ];
-
-  networking = {
-    hostName = "other"; # Define your hostname.
-  };
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # enable flakes
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "amdgpu" ];
-
-  # Configure keymap in X11
-  #services.xserver.xkbOptions = "caps:swapescape";
-  services.xserver.xkb = {
-    layout = "de";
-    variant = "";
-  };
-
-  # Configure console keymap
-  console.keyMap = lib.mkForce "de";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  #hardware.pulseaudio.enable = true;
-  security.rtkit.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-  nix.settings = {
-    auto-optimise-store = true;
-  };
+  console.keyMap = "de";
 
   users.defaultUserShell = pkgs.zsh;
   programs.zsh = {
@@ -88,23 +11,25 @@ in
     ohMyZsh = {
       enable = true;
       theme = "skaro";
-      #skaro  
-      #gentoo
     };
     shellAliases = {
       ga = "git add";
       gc = "git commit";
       gp = "git push";
-      gl = "git pull";
+      gf = "git fetch";
+      gcl = "git clone";
       nixin = "cd ~/nixsetup";
       nixbuild = "sudo nixos-rebuild switch --flake ~/nixsetup --impure";
       homebuild = "home-manager switch --flake ~/nixsetup --impure";
       c = "cd";
       cl = "clear";
-      nix-shell = "nix-shell --run $SHELL";
-      hibernate = "systemctl hibernate";
-      sleep = "systemctl suspend";
+      nsh = "nix shell";
     };
+  };
+
+  services.xserver.xkb = {
+    layout = "de";
+    options = "caps:swapescape";
   };
 
   programs.neovim = {
@@ -114,54 +39,16 @@ in
     vimAlias = true;
   };
 
-  console = {
-    useXkbConfig = true;
+  nix.settings = {
+    show-trace = true;
+
+    system-features = [ "big-parallel" "kvm" "recursive-nix" ];
+    sandbox-paths = [ "/bin/sh=${pkgs.busybox-sandbox-shell.out}/bin/busybox" ];
+
+    experimental-features = ["nix-command flakes"];
   };
-  users.users.uff20xd = {
-    isNormalUser = true;
-    description = "uff20xd";
-    extraGroups = [ "networkmanager" "wheel" "cdrom" ];
-  };
 
-	programs.firefox.enable = true;
-  nixpkgs.config.allowUnfree = true;
-
-  services.usbmuxd = {
-    enable = true;
-    package = pkgs.usbmuxd2;
-  };
-	environment.systemPackages = with pkgs; [
-
-    home-manager 
-    neovim
-    nnn
-
+  environment.systemPackages = with pkgs; [
+    git
   ];
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 3389 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
 }
